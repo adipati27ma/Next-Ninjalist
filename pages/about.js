684c4328a8f075e9e4684f7d52afc75b68a56e0f
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-03-29 15:41:38 +0700

    Pages & Routes

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+        consectetur voluptatibus? Cum veritatis veniam officiis tempore,
+        recusandae labore corporis possimus optio maiores culpa quisquam quia ex
+        nemo adipisci quas deleniti?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
+        consectetur voluptatibus? Cum veritatis veniam officiis tempore,
+        recusandae labore corporis possimus optio maiores culpa quisquam quia ex
+        nemo adipisci quas deleniti?
+      </p>
+    </div>
+  );
+};
+
+export default About;
