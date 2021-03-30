be649a061fe58d3acf46b1aaf0ea10d20d806f0f
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-03-31 01:00:40 +0700

    fetching single item/data

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,5 @@
+// func will tell Next.js how many html pages need to be made
+// based on the data (ninjas)
 export const getStaticPaths = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await res.json();
@@ -9,15 +11,30 @@ export const getStaticPaths = async () => {
   });
 
   return {
-    paths,
+    paths, // variable ini yg akan dilihat oleh Next.js
     fallback: false, // jika id yg diakses tidak ada, maka akan 404
   };
 };
 
-const Details = () => {
+// func akan di-run 10x, sesuai jumlah item di variabel "paths" (yg dibuat)
+// variable "paths" akan masuk ke "context"
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
+
+const Details = ({ ninja }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{ninja.name}</h1>
+      <p>Email : {ninja.email}</p>
+      <p>Website : {ninja.website}</p>
+      <p>City : {ninja.address.city}</p>
     </div>
   );
 };
